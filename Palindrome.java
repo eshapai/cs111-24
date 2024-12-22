f663bd82312fdec38b6702553fba4d6f05d082ec
eshapai <115886872+eshapai@users.noreply.github.com>
2024-12-21 23:40:18 -0500

    Add files via upload

---
+++ Palindrome.java
@@ -0,0 +1,32 @@
+/*
+ * Write the Palindrome program inside the main method
+ * according to the assignment description.
+ * 
+ * To compile:
+ *        javac Palindrome.java
+ * To execute:
+ *        java Palindrome 5 4 6 6 4 5
+ * 
+ * DO NOT change the class name
+ * DO NOT use System.exit()
+ * DO NOT change add import statements
+ * DO NOT add project statement
+ * 
+ */
+
+public class Palindrome 
+{
+    public static void main(String[] args) 
+    {
+       
+        int a = Integer.parseInt(args[0]);
+        int b = Integer.parseInt(args[1]);
+        int c = Integer.parseInt(args[2]);
+        int d = Integer.parseInt(args[3]);
+        int e = Integer.parseInt(args[4]);
+        int f = Integer.parseInt(args[5]);
+
+        boolean palindrome = a==(f) && b==(e) && c==(d);
+        System.out.println(palindrome);
+    }
+}
